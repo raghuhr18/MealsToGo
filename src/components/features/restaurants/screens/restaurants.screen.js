6f06ff0a1cab4bd96a32aeaa321651db66d235c1
raghuhr18 <raghuhr18@yahoo.com>
2023-07-28 23:55:28 +0530

    IOS fix

--- src/components/features/restaurants/screens/restaurants.screen.js
+++ src/components/features/restaurants/screens/restaurants.screen.js
@@ -8,7 +8,7 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card-component
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  marginTop: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight }px` };
 `;
 
 const SearchContainer = styled(View)`
